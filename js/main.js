c65344bea4349b9ac432763605c12a92706ab014
matszc <matiz305@hotmail.com>
2019-01-22 20:09:34 +0100

    fix sroll nav

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@ function scroll(link) {
     let target  = $(this).attr('href');
 
     link.preventDefault();
-
+    $('.navbar').removeClass('sticky-top');
     $('html, body').animate({
-        scrollTop: ($(target).offset().top)-74
+        scrollTop: ($(target).offset().top)
     }, 800);
+
+    $('.navbar').addClass('sticky-top');
 }
 
 $('.nav-link').click(scroll);
-$('.navbar-brand').click(scroll);
 
 })
